244afc9b4aea1e819f9f54561256ae663a238d61
unknown <onurcoskun99@gmail.com>
2020-12-14 20:24:52 +0300

    cors add

--- server.js
+++ server.js
@@ -9,9 +9,10 @@ const GridFsStorage = require('multer-gridfs-storage');
 const cookieParser = require('cookie-parser');
 const app = express();
 const port = process.env.PORT || 8000;
+const cors = require('cors');
 
 
-
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 app.use(cookieParser())
